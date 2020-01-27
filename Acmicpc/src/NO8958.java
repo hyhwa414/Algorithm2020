9100fdbfaafe63b5da1dd09cb79325ea31aeb60f
hyhwa414 <hyhwa414@gmail.com>
2020-01-28 00:53:42 +0900

    I solved a problem NO8958 from acmicpc

--- Acmicpc/src/NO8958.java
+++ Acmicpc/src/NO8958.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
 
 public class NO8958 {
-
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+	
+		int N = s.nextInt();
+		s.nextLine();
+		for(int i = 0; i < N; i++) {
+			char[] ch = s.nextLine().toCharArray();
+			int score = 0;
+			int sum = 0;
+			
+			for(int j = 0; j < ch.length; j++) {
+				if(score == 0 && ch[j] == 'O') { // first O
+					score = 1; 
+				} else if (score != 0 && ch[j] == 'O') {
+					score ++;
+				} else {
+					score = 0;
+				}
+				sum += score;
+			}
+			System.out.println(sum);
+		}
+	}
 }
