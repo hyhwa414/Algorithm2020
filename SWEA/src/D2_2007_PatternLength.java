649e0dd900a684346c6bcc966f229d05cfff7856
hyhwa414 <hyhwa414@gmail.com>
2020-01-29 23:01:41 +0900

    I solved a problem D2_2007_PatternLength from SWEA

---
+++ SWEA/src/D2_2007_PatternLength.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class D2_2007_PatternLength {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		sc.nextLine();
+		
+		for(int i = 0; i < T; i++) {
+			int patternLength = 0;
+			StringBuilder input = new StringBuilder(sc.nextLine());
+			
+			StringBuilder pattern = new StringBuilder(input.substring(0, 1)); // 첫문자 대입
+			input.delete(0, 1);
+			
+			int returnPoint = input.indexOf(pattern.toString());
+			pattern.append(input.substring(0, returnPoint));
+			input.delete(0, returnPoint);
+			
+			while(true) {
+				// 문제에 오류가 있음 cocoa 같은 테스트케이스는 co를 패턴으로 인식하기 때문에 fail이 떠야함
+				if(input.substring(0, pattern.length()).equals(pattern.toString())) {
+					patternLength = pattern.length();
+					break;
+				} else {
+					returnPoint = input.indexOf(pattern.toString());
+					pattern.append(input.substring(0, returnPoint));
+					input.delete(0, returnPoint);
+				}
+			}
+			
+			System.out.println("#"+(i+1)+" "+patternLength);
+		}
+	}
+}
