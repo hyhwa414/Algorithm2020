f00e1ebe4f07c488169db9d73593c6c771f1a4d2
hyhwa414 <hyhwa414@gmail.com>
2020-02-09 15:12:39 +0900

    I solved a problem No9095 from acmicpc

---
+++ Acmicpc/src/NO9095.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class NO9095 {
+	static int count;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int tc = sc.nextInt();
+		for(int i = 0; i < tc; i++) {
+			int num = sc.nextInt();
+			count = 0;
+			divide(num);
+			System.out.println(count);
+		}
+	}
+
+	public static void divide(int num) {
+		if(num == 0) {
+			count++;
+			return;
+		}
+		for(int i = 1; i <= num; i++) {
+			if(i > 3)
+				break;
+			divide(num - i);
+		}
+//		if(num >= 3) {
+//			divide(num - 3);
+//			divide(num - 2);
+//			divide(num - 1);
+//		} else if (num >= 2) {
+//			divide(num - 2);
+//			divide(num - 1);
+//		} else if (num >= 1) {
+//			divide(num - 1);
+//		} else if (num == 0){
+//			count++;
+//			return;
+//		}
+	}
+}
