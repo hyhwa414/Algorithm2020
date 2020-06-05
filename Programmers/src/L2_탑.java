4c849df4e9c847efe9e5c20cd07a61672ae0f800
hyhwa414 <hyhwa414@gmail.com>
2020-06-05 10:52:41 +0900

    I solved a problem L2_탑 from Programmers

---
+++ Programmers/src/L2_탑.java
@@ -0,0 +1,16 @@
+
+public class L2_탑 {
+	public int[] solution(int[] heights) {
+        int[] answer = new int[heights.length];
+        
+        for(int i = heights.length - 1; i >= 0 ; i--){
+            for(int j = i - 1; j >= 0; j--){
+                if(heights[i] < heights[j]){
+                    answer[i] = j + 1;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
