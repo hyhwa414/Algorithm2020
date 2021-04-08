96e4df7c15e13b8127ddef8bbc2a67806cae2d76
hyhwa414 <hyhwa414@gmail.com>
2021-04-08 14:13:09 +0900

    2021.04.08 백준 #1001 A-B

---
+++ Acmicpc/01_입출력과_사칙연산/No_1001_AminusB.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class No_1001_AminusB {
+	public static void main(String[] args) {
+		Scanner S = new Scanner(System.in);
+		
+		int A = S.nextInt();
+		int B = S.nextInt();
+		System.out.println(A - B);
+	}
+}
